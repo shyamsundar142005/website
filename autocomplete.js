aa58a4d3958de27f56466170d20103335c93b425
shyamsundar142005 <168510161+shyamsundar142005@users.noreply.github.com>
2024-08-13 22:37:44 +0530

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopupBtn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
